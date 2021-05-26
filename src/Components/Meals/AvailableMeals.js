11e8742393a56af2c4d787078e9f6026cabac08b
Nandan Reddy <nandan.reddy1133@gmail.com>
2021-05-26 21:33:16 +0530

    Basic copy
    For some reason git thinks the files are changed when I first opened after the creating a branch
    
    So I am keeping this as a base copy

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -1,53 +1,53 @@
-import Card from "../UI/Card";
-import classes from "./AvailableMeals.module.css";
-import MealItem from "./MealItem/MealItem";
+import Card from '../UI/Card';
+import classes from './AvailableMeals.module.css';
+import MealItem from './MealItem/MealItem';
 
 const DUMMY_MEALS = [
-    {
-        id: "m1",
-        name: "Sushi",
-        description: "Finest fish and veggies",
-        price: 1699,
-    },
-    {
-        id: "m2",
-        name: "Schnitzel",
-        description: "A german specialty!",
-        price: 1199,
-    },
-    {
-        id: "m3",
-        name: "Barbecue Burger",
-        description: "American, raw, meaty",
-        price: 899,
-    },
-    {
-        id: "m4",
-        name: "Green Bowl",
-        description: "Healthy...and green...",
-        price: 1399,
-    },
+	{
+		id: 'm1',
+		name: 'Sushi',
+		description: 'Finest fish and veggies',
+		price: 1699,
+	},
+	{
+		id: 'm2',
+		name: 'Schnitzel',
+		description: 'A german specialty!',
+		price: 1199,
+	},
+	{
+		id: 'm3',
+		name: 'Barbecue Burger',
+		description: 'American, raw, meaty',
+		price: 899,
+	},
+	{
+		id: 'm4',
+		name: 'Green Bowl',
+		description: 'Healthy...and green...',
+		price: 1399,
+	},
 ];
 
 function AvailableMeals() {
-    const mealsList = DUMMY_MEALS.map((meal) => {
-        return (
-            <MealItem
-                key = {meal.id}
-                id = {meal.id}
-                name={meal.name}
-                description={meal.description}
-                price={meal.price}
-            />
-        );
-    });
-    return (
-        <section className={classes.meals}>
-            <Card>
-                <ul>{mealsList}</ul>
-            </Card>
-        </section>
-    );
+	const mealsList = DUMMY_MEALS.map((meal) => {
+		return (
+			<MealItem
+				key={meal.id}
+				id={meal.id}
+				name={meal.name}
+				description={meal.description}
+				price={meal.price}
+			/>
+		);
+	});
+	return (
+		<section className={classes.meals}>
+			<Card>
+				<ul>{mealsList}</ul>
+			</Card>
+		</section>
+	);
 }
 
 export default AvailableMeals;
